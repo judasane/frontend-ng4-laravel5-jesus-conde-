c457e3da8101abaaff804c00d7bfefd6652eb739
judasane <judasane@gmail.com>
2018-05-14 04:13:19 -0500

    Cambiada la ruta del endpoint para funcionar con la api

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -6,25 +6,33 @@ import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class ImageService {
-  constructor(private http: Http) { }
+
+
+  
+  constructor(private http: Http) {
+
+    
+   }
 
   getImages(): Observable<Image[]> {
-    return this.http.get('http://127.0.0.1:8000/api/v1/images').map((response: Response) => response.json());
+
+    
+    return this.http.get('http://localhost/api-galeria/public/api/v1/images').map((response: Response) => response.json());
   }
 
   addImage(image: Object): Observable<Image[]> {
-    return this.http.post('http://127.0.0.1:8000/api/v1/images',image)
+    return this.http.post('http://localhost/api-galeria/public/api/v1/images',image)
       .map((response: Response) => response.json())
       .catch((error: any) => Observable.throw(error.json().error || { message: 'Error del servidor' }));
   }
 
   getImage(id: String): Observable<Image[]> {
-    return this.http.get('http://127.0.0.1:8000/api/v1/images/'+id)
+    return this.http.get('http://localhost/api-galeria/public/api/v1/images/'+id)
       .map((response: Response) => response.json())
   }
 
   updateImage(image: Object): Observable<Image[]> {
-  const apiUrl = 'http://127.0.0.1:8000/api/v1/images';
+  const apiUrl = 'http://localhost/api-galeria/public/api/v1/images';
   const url = `${apiUrl}/${image["id"]}`;
   return this.http.put(url, image)
     .map((response: Response) => response.json())
